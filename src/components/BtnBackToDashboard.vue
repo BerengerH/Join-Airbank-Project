24727f4534c635bf40f2460b6233742ff34e732d
BerengerH <berenger.humeau@yahoo.fr>
2022-10-31 13:48:51 +0100

    Creation of BtnBackToDashboard that allow user to go back to the dashboard page. Button added to the detailed transaction page

---
+++ src/components/BtnBackToDashboard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "BtnBackToDashboard",
+  methods: {
+    accessDashboard() {
+      this.$router.push({ path: "/" });
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+    @click="accessDashboard"
+    class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-1 px-3 rounded"
+  >
+    Back to dashboard
+  </button>
+</template>
--- src/pages/DetailedTransaction.vue
+++ src/pages/DetailedTransaction.vue
@@ -1,29 +1,41 @@
-<script></script>
+<script>
+import BtnBackToDashboard from "../components/BtnBackToDashboard.vue";
+
+export default {
+  name: "DetailedTransaction",
+  components: {
+    BtnBackToDashboard,
+  },
+};
+</script>
 
 <template>
-  <section class="m-[5%] bg-white p-5 flex flex-wrap justify-between gap-4">
-    <div class="flex flex-col gap-4">
-      <div class="flex flex-wrap gap-0 items-center sm:gap-2">
-        <h3 class="font-bold">Transaction's reference:</h3>
-        <p class="text-sm">purchase (info will be fetched)</p>
+  <section class="m-[5%]">
+    <BtnBackToDashboard />
+    <div class="mt-[3%] bg-white p-5 flex flex-wrap justify-between gap-4">
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-wrap gap-0 items-center sm:gap-2">
+          <h3 class="font-bold">Transaction's reference:</h3>
+          <p class="text-sm">purchase (info will be fetched)</p>
+        </div>
+        <div class="flex flex-wrap gap-0 items-center sm:gap-2">
+          <h3 class="font-bold">Transaction's category:</h3>
+          <p class="text-sm">business (info will be fetched)</p>
+        </div>
+        <div class="flex flex-wrap gap-0 items-center sm:gap-2">
+          <h3 class="font-bold">Transaction's date:</h3>
+          <p class="text-sm">2017-09-01 (info will be fetched)</p>
+        </div>
       </div>
-      <div class="flex flex-wrap gap-0 items-center sm:gap-2">
-        <h3 class="font-bold">Transaction's category:</h3>
-        <p class="text-sm">business (info will be fetched)</p>
-      </div>
-      <div class="flex flex-wrap gap-0 items-center sm:gap-2">
-        <h3 class="font-bold">Transaction's date:</h3>
-        <p class="text-sm">2017-09-01 (info will be fetched)</p>
-      </div>
-    </div>
-    <div class="flex flex-col gap-4">
-      <div class="flex flex-wrap gap-0 items-center sm:gap-2">
-        <h3 class="font-bold">Transaction's amount:</h3>
-        <p class="text-sm">-100 (info will be fetched)</p>
-      </div>
-      <div class="flex flex-wrap gap-0 items-center sm:gap-2">
-        <h3 class="font-bold">Transaction's currency:</h3>
-        <p class="text-sm">EUR (info will be fetched)</p>
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-wrap gap-0 items-center sm:gap-2">
+          <h3 class="font-bold">Transaction's amount:</h3>
+          <p class="text-sm">-100 (info will be fetched)</p>
+        </div>
+        <div class="flex flex-wrap gap-0 items-center sm:gap-2">
+          <h3 class="font-bold">Transaction's currency:</h3>
+          <p class="text-sm">EUR (info will be fetched)</p>
+        </div>
       </div>
     </div>
   </section>
